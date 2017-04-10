72d57f1b11d5cefb3e8f09a20f68ad60ae550b9f
yaqinh <yaqinh@bu.edu>
2017-04-10 12:02:54 -0400

    single player web finished

---
+++ FIFAIndexWebScarper/fifaiscrap.py
@@ -0,0 +1,287 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+import re
+import time
+import pandas as pd
+import numpy as np
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+driver = webdriver.Chrome() #initialize the webdriver
+
+def singlePlayer(url):
+    driver.get(url)
+    singleList = []
+    
+    name = '?'
+    country = '?'
+    overallScore = '?'
+    potentialScore = '?'
+    Height = '?'
+    Weight = '?'
+    PreferredFoot = '?'
+    BirthDate = '?'
+    Age = '?'
+    PreferredPositions = '?'
+    PlayerWorkRate = '?'
+    team = '?'
+    teamPosition = '?'
+    year = '?'
+    contract = '?'
+    BallControl = '?'
+    Dribbling = '?'
+    Marking = '?'
+    SlideTackle = '?'
+    StandTackle = '?'
+    Aggression = '?'
+    Reactions = '?'
+    AttPosition = '?'
+    Interceptions = '?'
+    Vision = '?'
+    Composure = '?'
+    Crossing = '?'
+    ShortPass = '?'
+    LongPass = '?'
+    Acceleration = '?'
+    Stamina = '?'
+    Strength = '?'
+    Balance = '?'
+    SprintSpeed = '?'
+    Agilityd = '?'
+    Jumping = '?'
+    Heading = '?'
+    ShotPower = '?'
+    Finishing = '?'
+    LongShots = '?'
+    Curve = '?'
+    FKAcc = '?'
+    Penalties = '?'
+    Volleys = '?'
+    GKPositioning = '?'
+    GKDiving = '?'
+    GKHandling = '?'
+    GKKicking = '?'
+    GKReflexes = '?'
+    
+    try: #get basic infomation
+        basicInfo = driver.find_element_by_xpath("""/html/body/div[3]/div[2]/div[2]/div[1]""").text.split('\n')
+        name = basicInfo[0]
+        country = basicInfo[1]
+        overallScore = basicInfo[3].split()[0]
+        potentialScore = basicInfo[3].split()[1]
+        for index1 in range(len(basicInfo)):
+            if re.search(r'\bHeight\b', basicInfo[index1]):
+                Height = basicInfo[index1 + 1]
+        
+            if re.search(r'\bWeight\b', basicInfo[index1]):
+                Weight = basicInfo[index1 + 1]
+        
+            if re.search(r'\bPreferred Foot\b', basicInfo[index1]):
+                PreferredFoot = basicInfo[index1 + 1]
+        
+            if re.search(r'\bBirth Date\b', basicInfo[index1]):
+                BirthDate = basicInfo[index1 + 1]
+        
+            if re.search(r'\bAge\b', basicInfo[index1]):
+                Age = basicInfo[index1 + 1]
+        
+            if re.search(r'\bPreferred Positions\b', basicInfo[index1]):
+                PreferredPositions = basicInfo[index1 + 1]
+        
+            if re.search(r'\bPlayer Work Rate\b', basicInfo[index1]):
+                PlayerWorkRate = basicInfo[index1 + 1]
+    except:
+        print(url + ' basic info not found')
+
+    #print(basicInfo)
+    singleList.append(name)
+    singleList.append(country)
+    singleList.append(overallScore)
+    singleList.append(potentialScore)
+    singleList.append(Height)
+    singleList.append(Weight)
+    singleList.append(PreferredFoot)
+    singleList.append(BirthDate)
+    singleList.append(Age)
+    singleList.append(PreferredPositions)
+    singleList.append(PlayerWorkRate)
+    
+    
+    try: #get team infomation
+        teamInfo = driver.find_element_by_xpath("""/html/body/div[3]/div[2]/div[2]/div[2]""").text.split('\n')
+        team = teamInfo[5]
+        for index2 in range(len(teamInfo)):
+            if re.search(r'\bPosition\b', teamInfo[index2]):
+                teamPosition = teamInfo[index2 + 1] 
+        
+            if re.search(r'\bJoined Club\b', teamInfo[index2]):
+                year = teamInfo[index2 + 1]
+        
+            if re.search(r'\bContract Length\b', teamInfo[index2]):
+                contract = teamInfo[index2 + 1]
+    except:
+        print(url + ' team info not found')
+        
+    #print(teamInfo)
+    singleList.append(team)
+    singleList.append(teamPosition)
+    singleList.append(year)
+    singleList.append(contract)
+
+    
+    try: #get performance infomation
+        performInfo = driver.find_element_by_xpath("""/html/body/div[3]/div[2]/div[2]/div[3]""").text.split('\n')
+        for index3 in range(len(performInfo)):
+            if re.search(r'\bBall Control\b', performInfo[index3]):
+                BallControl = performInfo[index3 + 1]
+        
+            if re.search(r'\bDribbling\b', performInfo[index3]):
+                Dribbling = performInfo[index3 + 1]
+    
+            if re.search(r'\bMarking\b', performInfo[index3]):
+                Marking = performInfo[index3 + 1]
+        
+            if re.search(r'\bSlide Tackle\b', performInfo[index3]):
+                SlideTackle = performInfo[index3 + 1]
+        
+            if re.search(r'\bStand Tackle\b', performInfo[index3]):
+                StandTackle = performInfo[index3 + 1]
+        
+            if re.search(r'\bAggression\b', performInfo[index3]):
+                Aggression = performInfo[index3 + 1]
+        
+            if re.search(r'\bReactions\b', performInfo[index3]):
+                Reactions = performInfo[index3 + 1]
+        
+            if re.search(r'\bAtt. Position\b', performInfo[index3]):
+                AttPosition = performInfo[index3 + 1]
+        
+            if re.search(r'\bInterceptions\b', performInfo[index3]):
+                Interceptions = performInfo[index3 + 1]
+        
+            if re.search(r'\bVision\b', performInfo[index3]):
+                Vision = performInfo[index3 + 1]
+        
+            if re.search(r'\bComposure\b', performInfo[index3]):
+                Composure = performInfo[index3 + 1]
+        
+            if re.search(r'\bCrossing\b', performInfo[index3]):
+                Crossing = performInfo[index3 + 1]
+        
+            if re.search(r'\bShort Pass\b', performInfo[index3]):
+                ShortPass = performInfo[index3 + 1]
+        
+            if re.search(r'\bLong Pass\b', performInfo[index3]):
+                LongPass = performInfo[index3 + 1]
+        
+            if re.search(r'\bAcceleration\b', performInfo[index3]):
+                Acceleration = performInfo[index3 + 1]
+        
+            if re.search(r'\bStamina\b', performInfo[index3]):
+                Stamina = performInfo[index3 + 1]
+        
+            if re.search(r'\bStrength\b', performInfo[index3]):
+                Strength = performInfo[index3 + 1]
+        
+            if re.search(r'\bBalance\b', performInfo[index3]):
+                Balance = performInfo[index3 + 1]
+        
+            if re.search(r'\bSprint Speed\b', performInfo[index3]):
+                SprintSpeed = performInfo[index3 + 1]
+        
+            if re.search(r'\bAgility\b', performInfo[index3]):
+                Agilityd = performInfo[index3 + 1]
+        
+            if re.search(r'\bJumping\b', performInfo[index3]):
+                Jumping = performInfo[index3 + 1]
+        
+            if re.search(r'\bHeading\b', performInfo[index3]):
+                Heading = performInfo[index3 + 1]
+        
+            if re.search(r'\bShot Power\b', performInfo[index3]):
+                ShotPower = performInfo[index3 + 1]
+        
+            if re.search(r'\bFinishing\b', performInfo[index3]):
+                Finishing = performInfo[index3 + 1]
+        
+            if re.search(r'\bLong Shots\b', performInfo[index3]):
+                LongShots = performInfo[index3 + 1]
+        
+            if re.search(r'\bCurve\b', performInfo[index3]):
+                Curve = performInfo[index3 + 1]
+        
+            if re.search(r'\bFK Acc\b', performInfo[index3]):
+                FKAcc = performInfo[index3 + 1]
+        
+            if re.search(r'\bPenalties\b', performInfo[index3]):
+                Penalties = performInfo[index3 + 1] 
+        
+            if re.search(r'\bVolleys\b', performInfo[index3]):
+                Volleys = performInfo[index3 + 1]
+        
+            if re.search(r'\bGK Positioning\b', performInfo[index3]):
+                GKPositioning = performInfo[index3 + 1]
+        
+            if re.search(r'\bGK Diving\b', performInfo[index3]):
+                GKDiving = performInfo[index3 + 1]
+        
+            if re.search(r'\bGK Handling\b', performInfo[index3]):
+                GKHandling = performInfo[index3 + 1]
+        
+            if re.search(r'\bGK Kicking\b', performInfo[index3]):
+                GKKicking = performInfo[index3 + 1]
+        
+            if re.search(r'\bGK Reflexes\b', performInfo[index3]):
+                GKReflexes = performInfo[index3 + 1]
+    except:
+        print(url + ' performance info not found') 
+    
+    singleList.append(BallControl)
+    singleList.append(Dribbling)
+    singleList.append(Marking)
+    singleList.append(SlideTackle)
+    singleList.append(StandTackle)
+    singleList.append(Aggression)
+    singleList.append(Reactions)
+    singleList.append(AttPosition)
+    singleList.append(Interceptions)
+    singleList.append(Vision)
+    singleList.append(Composure)
+    singleList.append(Crossing)
+    singleList.append(ShortPass)
+    singleList.append(LongPass)
+    singleList.append(Acceleration)
+    singleList.append(Stamina)
+    singleList.append(Strength)
+    singleList.append(Balance)
+    singleList.append(SprintSpeed)
+    singleList.append(Agilityd)
+    singleList.append(Jumping)
+    singleList.append(Heading)
+    singleList.append(ShotPower)
+    singleList.append(Finishing)
+    singleList.append(LongShots)
+    singleList.append(Curve)
+    singleList.append(FKAcc)
+    singleList.append(Penalties)
+    singleList.append(Volleys)
+    singleList.append(GKPositioning)
+    singleList.append(GKDiving)
+    singleList.append(GKHandling)
+    singleList.append(GKKicking)
+    singleList.append(GKReflexes)
+    #print(performInfo)
+    
+    return singleList
+
+singleList = singlePlayer('https://www.fifaindex.com/player/20801/cristiano-ronaldo/')
+header = ['Name', 'Country', 'OverallScore', 'PotentialScore', 'Height', 'Weight', 'PreferredFoot', 'BirthDate', 
+          'Age', 'PreferredPositions', 'PlayerWorkRate', 'Team', 'TeamPosition', 'Year', 'Contract', 'BallControl',
+          'Dribbling', 'Marking', 'SlideTackle', 'StandTackle', 'Aggression', 'Reactions', 'AttPosition', 'Interceptions',
+          'Vision', 'Composure', 'Crossing', 'ShortPass', 'LongPass', 'Acceleration', 'Stamina', 'Strength', 'Balance',
+          'SprintSpeed', 'Agilityd', 'Jumping', 'Heading', 'ShotPower', 'Finishing', 'LongShots', 'Curve', 'FKAcc', 
+          'Penalties', 'Volleys', 'GKPositioning', 'GKDiving', 'GKHandling', 'GKKicking', 'GKReflexes']
+playerList = []
+playerList.append(singleList)
+players = pd.DataFrame(playerList, columns=header)
+players
